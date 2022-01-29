d32f0c48d54bc315fd64a0a73444206ddeeb3eff
Jan Behrens <dev@jpbehrens.de>
2022-01-29 22:49:30 +0100

    refactored Routes

--- routes/api.php
+++ routes/api.php
@@ -25,10 +25,20 @@ use App\Http\Controllers\ProductController;
 Route::resource('products', ProductController::class);
 # Route um Produkte via Name zu finden
 
+# Aufteilen der Routen 
+# Public
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
+# Private with Authentication
 Route::group(['middleware'=>['auth:sanctum']], function(){
-    Route::get('/products/search/{name}', [ProductController::class, 'search']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+# Not used LARAVEL DEFAULT 
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
