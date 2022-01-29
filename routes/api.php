87b13cfc1c30a22278e957aaecf4331c6c082054
Jan Behrens <dev@jpbehrens.de>
2022-01-29 16:49:24 +0100

    Refactored: Routes and Controller

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Product;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,20 +15,8 @@ use App\Models\Product;
 */
 
 # ROUTE = http://localhost:8000/api/products
-Route::get('/products', function(){
-    // Zeigt alle Produkte an
-    return Product::all();
-});
-
-Route::post('/products', function(){
-   // Erstellt ein Produkt
-   return Product::create([
-       'name' => 'Product One',
-       'slug' => 'product-one',
-       'description' => 'This is Product One',
-       'price' => '99.95'
-   ]);  
-});
+Route::get('/products',[ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
