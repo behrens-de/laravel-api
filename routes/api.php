4059db4ffe3c3405e09967dcf695b4ebeab2aab1
Jan Behrens <dev@jpbehrens.de>
2022-01-29 23:35:44 +0100

    created AuthController and register Route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -27,6 +28,7 @@ Route::resource('products', ProductController::class);
 
 # Aufteilen der Routen 
 # Public
+Route::post('/register', [AuthController::class, 'register']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
