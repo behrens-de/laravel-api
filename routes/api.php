4724c5c87c4cbe69690189e2a67aa687f0cf556d
Jan Behrens <dev@jpbehrens.de>
2022-01-29 23:46:30 +0100

    added logout Method and Route

--- routes/api.php
+++ routes/api.php
@@ -38,6 +38,7 @@ Route::group(['middleware'=>['auth:sanctum']], function(){
     Route::post('/products', [ProductController::class, 'store']);
     Route::put('/products/{id}', [ProductController::class, 'update']);
     Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::post('/logout',[AuthController::class,'logout']);
 });
 
 # Not used LARAVEL DEFAULT 
